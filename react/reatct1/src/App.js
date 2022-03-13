6ac83a4580d319a9d6df1a39d2d0ca1bc69b4554
madhavirabbagungu@gmail.com <madhavirabbgundu@gmail.com>
2022-03-14 00:47:05 +0530

    changes

---
+++ react/reatct1/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+ import { list } from "./components/list.jsx";
+
+
+function App() {
+  return (
+     <div className="App">
+    
+    
+    
+           <h1> Mobile Manufacturers</h1>
+           
+           {/* <ul>
+            <li>item1</li>
+            <li>item2</li>
+            <li>item3</li>
+            <li>item4 </li>
+        </ul> */}
+            < list/> 
+     </div>
+
+  );
+}
+
+export default App;
